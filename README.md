d6718d865549c92df7159c995ff428aed9d27bf1
Elin571975 <elin571975@gmail.com>
2019-03-25 18:23:46 -0400

    Mar.25.2019

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# FriendsFinder
+# Friend Finder - Node and Express Servers
+
+### Overview
+
+It is compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your survey, then compare your answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+
+### How Does Friends Finder Work
+
+1. The survey has 10 questions of, the answer should be on a scale of 1 to 5 based on how much you agree or disagree with a question. All questions must be answered.
+
+
+2. After submission of survey, app will determine the your most compatible friend. Your friend's name and picture will be displayed.
+
+Have Fun!!
---
+++ app/data/friends.js
@@ -0,0 +1,226 @@
+[
+    {
+      "name": "John",
+      "photo": "http://www.fillmurray.com/900/300",
+      "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+    {
+      "name": "Mary",
+      "photo": "http://www.fillmurray.com/600/480",
+      "scores": [
+        2,
+        4,
+        3,
+        4,
+        2,
+        1,
+        5,
+        1,
+        3,
+        4
+      ]
+    },
+    {
+      "name": "Brad",
+      "photo": "http://www.fillmurray.com/540/753",
+      "scores": [
+        4,
+        2,
+        1,
+        3,
+        4,
+        1,
+        1,
+        5,
+        4,
+        3
+      ]
+    },
+    {
+      "name": "Carmen",
+      "photo": "http://www.placecage.com/450/620",
+      "scores": [
+        1,
+        3,
+        3,
+        2,
+        5,
+        4,
+        5,
+        1,
+        3,
+        1
+      ]
+    },
+    {
+      "name": "Denise",
+      "photo": "http://www.placecage.com/900/300",
+      "scores": [
+        3,
+        1,
+        5,
+        2,
+        3,
+        2,
+        5,
+        2,
+        2,
+        5
+      ]
+    },
+    {
+      "name": "James",
+      "photo": "http://www.placecage.com/640/480",
+      "scores": [
+        2,
+        2,
+        5,
+        5,
+        3,
+        2,
+        2,
+        5,
+        4,
+        2
+      ]
+    },
+    {
+      "name": "Danny",
+      "photo": "http://www.fillmurray.com/270/320",
+      "scores": [
+        4,
+        4,
+        3,
+        1,
+        2,
+        3,
+        4,
+        4,
+        5,
+        2
+      ]
+    },
+    {
+      "name": "Paul",
+      "photo": "http://www.fillmurray.com/545/450",
+      "scores": [
+        5,
+        1,
+        1,
+        2,
+        4,
+        4,
+        3,
+        5,
+        4,
+        3
+      ]
+    },
+    {
+      "name": "Peter",
+      "photo": "http://www.fillmurray.com/768/412",
+      "scores": [
+        2,
+        5,
+        2,
+        5,
+        1,
+        4,
+        5,
+        3,
+        3,
+        5
+      ]
+    },
+    {
+      "name": "Shirley",
+      "photo": "http://www.stevensegallery.com/200/300",
+      "scores": [
+        2,
+        4,
+        2,
+        1,
+        5,
+        4,
+        2,
+        4,
+        5,
+        4
+      ]
+    },
+    {
+      "name": "Tara",
+      "photo": "http://www.fillmurray.com/460/300",
+      "scores": [
+        5,
+        4,
+        4,
+        2,
+        2,
+        2,
+        4,
+        2,
+        1,
+        1
+      ]
+    },
+    {
+      "name": "Parker",
+      "photo": "http://www.fillmurray.com/900/600",
+      "scores": [
+        5,
+        4,
+        4,
+        1,
+        2,
+        3,
+        5,
+        2,
+        1,
+        1
+      ]
+    },
+    {
+      "name": "Olivia",
+      "photo": "http://www.fillmurray.com/500/600",
+      "scores": [
+        5,
+        5,
+        5,
+        5,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1
+      ]
+    },
+    {
+      "name": "Ellito",
+      "photo": "http://www.fillmurray.com/500/700",
+      "scores": [
+        3,
+        3,
+        3,
+        2,
+        1,
+        5,
+        5,
+        1,
+        1,
+        1
+      ]
+    }
+  ]
